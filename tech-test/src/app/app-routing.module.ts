b0ae6157f84b339628ca3a2ae48d712086d8b810
razjoorez <46075716+razjoorez@users.noreply.github.com>
2021-03-09 17:40:51 +0000

    main comp

--- tech-test/src/app/app-routing.module.ts
+++ tech-test/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'TaskList', loadChildren: () => import('./task-list/task-list.module').then(m => m.TaskListModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
